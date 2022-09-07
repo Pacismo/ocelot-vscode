c4336f3bf77367ada7f63202d67d6317add8cbee
Francisco Santana <francisco.santana2002@gmail.com>
2022-09-07 12:13:06 -0400

    Fiddling with the tutorial

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('ocelot-vscode.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ocelot-vscode!');
+		vscode.window.showInformationMessage('Wow, is this a mod for the template?!');
 	});
 
 	context.subscriptions.push(disposable);
